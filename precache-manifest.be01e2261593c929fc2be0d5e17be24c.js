5638edfb28343581bd00c0a3d4c7011abd537bb0
Raul Barriga <barrigaraulaug@gmail.com>
2020-09-24 11:50:12 -0700

    Updates

---
+++ precache-manifest.be01e2261593c929fc2be0d5e17be24c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b306f5ab1795a8b9fc3782288568fd34",
+    "url": "/Social-Media-Project/index.html"
+  },
+  {
+    "revision": "59c6546e991d91128401",
+    "url": "/Social-Media-Project/static/css/main.27716fba.chunk.css"
+  },
+  {
+    "revision": "d7af876bdc4aeddf8bc4",
+    "url": "/Social-Media-Project/static/js/2.dcc14eb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Social-Media-Project/static/js/2.dcc14eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c6546e991d91128401",
+    "url": "/Social-Media-Project/static/js/main.e00a8ba3.chunk.js"
+  },
+  {
+    "revision": "51213257b776eb7871e3",
+    "url": "/Social-Media-Project/static/js/runtime-main.18adaa7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Social-Media-Project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9b06277e042e5560b7d2c62d0e6e3d81",
+    "url": "/Social-Media-Project/static/media/meme.9b06277e.webp"
+  }
+]);
